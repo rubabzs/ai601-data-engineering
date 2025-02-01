7eb52e95b2f56ebd9b933615e1ad3a1dbbfc4791
Adil Mubashir Chaudhry <adil.mubashir@gmail.com>
2025-02-01 16:38:03 +0500

    Lab 1 - group10

--- labs/lab1/weather_data_pipeline.py
+++ labs/lab1/weather_data_pipeline.py
@@ -1,24 +1,41 @@
 import requests
 import csv
+import pandas as pd
 
-URL = ""
-
+#URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&past_days=10&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m'
 ### Part 1. Read Operation (Extract)
 def fetch_weather_data():
     """Fetches weather data for the past 10 days."""
     response = requests.get(URL)
 
     ## TODO: complete the code, the output should be data in json format
+    response_json = response.json()
+    #print(response_json)
+    return response_json
 
 
 ### Part 2. Write Operation (Load)
 def save_to_csv(data, filename):
     """Saves weather data to a CSV file."""
-    with open(filename, "<ENTER MODE HERE>", newline='', encoding='utf-8') as file:
-
+    with open(filename, "w", newline='', encoding='utf-8') as file:
         ### TODO: complete rest of the code, HINT: write the header row and body separately
+        writer = csv.writer(file, delimiter=",")
+        writer.writerow(['DateTime', 'Temp', 'Humid', 'Wind Speed'])
 
-        return None
+        t = data['hourly']['time']
+        temp = data['hourly']['temperature_2m']
+        h = data['hourly']['relative_humidity_2m']
+        w = data['hourly']['wind_speed_10m']
+
+        for i in range(len(t)):
+            writer.writerow([t[i], temp[i], h[i], w[i]])
+    #with open('PeshVsQuetta.json', encoding='utf-8-sig') as f_input:
+    #df = pd.DataFrame(data)
+    #print(df.head())
+
+    #df.to_csv(filename, encoding='utf-8', index=False)
+    return None
 
 ### Part 3. Cleaning Operation (Transform)
 def clean_data(input_file, output_file):
@@ -67,7 +84,7 @@ if __name__ == "__main__":
     weather_data = fetch_weather_data()
     if weather_data:
         save_to_csv(weather_data, "weather_data.csv")
-        print("Weather data saved to weather_data.csv")
+        #print("Weather data saved to weather_data.csv")
         #clean_data("weather_data.csv", "cleaned_data.csv")
         #print("Weather data clean saved to cleaned_data.csv")
         #summarize_data("cleaned_data.csv")
