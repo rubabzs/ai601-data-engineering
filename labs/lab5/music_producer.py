64bdd3cda63988fea3020359cf20a0e762ee13bd
Yahya Habib <yahyahabib65@gmail.com>
2025-03-18 01:45:32 +0500

    Lab 5

--- labs/lab5/music_producer.py
+++ labs/lab5/music_producer.py
@@ -8,19 +8,20 @@ from kafka import KafkaProducer
 
 TOPIC = "music_events"
 producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
+    bootstrap_servers="172.25.0.12:9092",
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
 songs = [101, 202, 303, 404, 505]   # sample song IDs
 regions = ["US", "EU", "APAC"]
+actions = ["play", "skip", "like"]
 
 while True:
     event = {
         "song_id": random.choice(songs),
         "timestamp": time.time(),
         "region": random.choice(regions),
-        "action": "play"  # or skip, etc.
+        "action": random.choice(actions)  # or skip, etc.
     }
     producer.send(TOPIC, event)
     print(f"Sent event: {event}")
